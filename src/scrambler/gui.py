24d43b6556edc93d3dd776389752ffa1f20f52f6
skl.progs <skl.progs@gmail.com>
2020-04-23 14:48:20 +0300

    Use stable shared import

--- src/scrambler/gui.py
+++ src/scrambler/gui.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-import skl_shared2.shared as sh
-from skl_shared2.localize import _
+import skl_shared.shared as sh
+from skl_shared.localize import _
 
 ICON = sh.objs.get_pdir().add('..','resources','icon_64x64_solve.gif')
 
